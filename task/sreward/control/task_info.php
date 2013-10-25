b6e524b8a87494577ce6f9115e6d386505fb6ce4
heavenK <heaven@we54.com>
2013-10-25 16:00:13 +0800

    Signed-off-by: heavenK <heaven@we54.com>

--- task/sreward/control/task_info.php
+++ task/sreward/control/task_info.php
@@ -54,9 +54,9 @@ if($days > $basic_config['vote_time'] && $task_info['is_vote'] == 2){
 	
 	db_factory::execute ( sprintf ( " update %switkey_task set is_vote=1 where task_id ='%d'", TABLEPRE, $task_id ) );
 
-	$works_list = db_factory::query ( sprintf ( "select * from %switkey_task_work where task_id=%d and vote_num > 0 order by vote_num desc limit 0,3", TABLEPRE, $task_id) );
+	$works_list = db_factory::query ( sprintf ( "select * from %switkey_task_work where task_id=%d and vote_num > 0 order by vote_num desc limit 0,2", TABLEPRE, $task_id) );
 	
-	$works_list_count = db_factory::get_count ( sprintf ( "select count(work_id) count from %switkey_task_work where task_id=%d and vote_num > 0 order by vote_num desc limit 0,3", TABLEPRE, $task_id) );
+	$works_list_count = db_factory::get_count ( sprintf ( "select count(work_id) count from %switkey_task_work where task_id=%d and vote_num > 0 order by vote_num desc limit 0,2", TABLEPRE, $task_id) );
 	
 	foreach($works_list as $key => $val){
 		keke_finance_class::cash_in($val['uid'], floatval(0),intval($basic_config['baoming_credit']),'vote_win','','vote_win');
