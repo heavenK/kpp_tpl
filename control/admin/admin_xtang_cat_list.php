33d590d4944aaf39d6cc58102f4b3e3abf35e62a
heavenK <heaven@we54.com>
2013-10-25 14:57:04 +0800

    Signed-off-by: heavenK <heaven@we54.com>

--- control/admin/admin_xtang_cat_list.php
+++ control/admin/admin_xtang_cat_list.php
@@ -1,10 +1,16 @@
 <?php
 defined ( 'ADMIN_KEKE' ) or exit ( 'Access Denied' );
 $url = 'index.php?do=xtang&view=cat_list';
-$cat_arr = db_factory::query ( sprintf ( "select * from %sxtang_type order by id asc, list_order asc", TABLEPRE ) );
+$cat_arr_1 = db_factory::query ( sprintf ( "select * from %sxtang_type where pid=0 order by list_order asc", TABLEPRE ) );
+$cat_arr_2 = db_factory::query ( sprintf ( "select * from %sxtang_type where pid>0 order by list_order asc", TABLEPRE ) );
+
 
 $cat_show_arr = array();
-foreach($cat_arr as $key => $val){
+foreach($cat_arr_1 as $key => $val){
+	if($val['pid'] == 0) $cat_show_arr[$val['id']] = $val;
+	else $cat_show_arr[$val['pid']]['sub'][$key] = $val;
+}
+foreach($cat_arr_2 as $key => $val){
 	if($val['pid'] == 0) $cat_show_arr[$val['id']] = $val;
 	else $cat_show_arr[$val['pid']]['sub'][$key] = $val;
 }
