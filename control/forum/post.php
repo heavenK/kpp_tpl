b8419fed0c544fea068fc8188c47e165010f1180
heavenK <heaven@we54.com>
2013-10-31 11:07:14 +0800

    Signed-off-by: heavenK <heaven@we54.com>

--- control/forum/post.php
+++ control/forum/post.php
@@ -4,6 +4,11 @@ if(!$uid) kekezu::show_msg("您还没有登录，无法发话题！",'index.php?
 
 $thread_right_arr = db_factory::query ( "select * from ".TABLEPRE."forum_thread where flag like '%r%' and status=2 and isShow=1 limit 0,10");
 
+if($tid){
+	$thread_info = db_factory::get_one(" select * from ".TABLEPRE."forum_thread where tid=".$tid);
+	
+	
+}
 
 if($sbt_edit){
 	
@@ -29,18 +34,30 @@ if($sbt_edit){
 	if($type == 'thread'){
 		if(!$title) kekezu::show_msg("标题不可以为空！",$url,3,'','error');
 		
-		$thread = db_factory::get_one(" select * from ".TABLEPRE."forum_thread order by tid desc limit 0,1");
-		
-		if($thread) $tid = $thread['tid']+1;
-		else $tid = 1;
-		
-		$res = db_factory::execute(" insert into ".TABLEPRE."forum_thread (tid,type_id, title, content, uid, username, pub_time, views, reply, isShow, status) value(".$tid.",".$type_id.",'".$title."','".$content."',".$uid.",'".$username."',".time().",0,0,1,2)");
+		if($tid){
+			$res = db_factory::execute(" update ".TABLEPRE."forum_thread set title='".$title."', content='".$content."' where tid=".$tid);
+				
+			if($res)	$res1 = db_factory::execute(" update ".TABLEPRE."forum_post set title='".$title."', content='".$content."' where tid=".$tid." and floor=1");
+			else	kekezu::show_msg("修改失败！",$url,3,'','error');
 			
-		if($res)	$res1 = db_factory::execute(" insert into ".TABLEPRE."forum_post (tid, type_id, title, content, uid, username, pub_time, status, floor) value(".$tid.",".$type_id.",'".$title."','".$content."',".$uid.",'".$username."',".time().",2, 1)");
-		else	kekezu::show_msg("发布失败！",$url,3,'','error');
+			if($res1)	kekezu::show_msg("修改成功！",$url_success.$tid,3,'','success');
+			else	kekezu::show_msg("修改失败！",$url,3,'','error');
+			
+		}else{
 		
-		if($res1)	kekezu::show_msg("发布成功！",$url_success.$tid,3,'','success');
-		else	kekezu::show_msg("发布失败！",$url,3,'','error');
+			$thread = db_factory::get_one(" select * from ".TABLEPRE."forum_thread order by tid desc limit 0,1");
+			
+			if($thread) $tid = $thread['tid']+1;
+			else $tid = 1;
+			
+			$res = db_factory::execute(" insert into ".TABLEPRE."forum_thread (tid,type_id, title, content, uid, username, pub_time, views, reply, isShow, status) value(".$tid.",".$type_id.",'".$title."','".$content."',".$uid.",'".$username."',".time().",0,0,1,2)");
+				
+			if($res)	$res1 = db_factory::execute(" insert into ".TABLEPRE."forum_post (tid, type_id, title, content, uid, username, pub_time, status, floor) value(".$tid.",".$type_id.",'".$title."','".$content."',".$uid.",'".$username."',".time().",2, 1)");
+			else	kekezu::show_msg("发布失败！",$url,3,'','error');
+			
+			if($res1)	kekezu::show_msg("发布成功！",$url_success.$tid,3,'','success');
+			else	kekezu::show_msg("发布失败！",$url,3,'','error');
+		}
 	}
 	if($type == 'post'){
 		
